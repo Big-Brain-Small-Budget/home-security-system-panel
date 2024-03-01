d858e3485e354f7651c2d5e9b769b48482901ab1
Patryk <114389730+pkatryk1707@users.noreply.github.com>
2024-03-01 10:24:07 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,36 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (armed == 0) {
+        currentSignal = receivedNumber
+    }
+    if (armed == 1) {
+        if (0 == 0) {
+        	
+        }
+    }
+})
 input.onButtonPressed(Button.A, function () {
     music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.InBackground)
     basic.showString("ARM")
     armed = 1
 })
-input.onGesture(Gesture.TiltLeft, function () {
+function activate () {
     if (armed == 1) {
         armed = 0
         active = 1
     }
-})
+}
 input.onButtonPressed(Button.AB, function () {
     basic.showString("OFF")
     active = 0
 })
 input.onButtonPressed(Button.B, function () {
-    if (armed == 1) {
-        armed = 0
-        active = 1
-    }
+    activate()
 })
+let currentSignal = 0
 let active = 0
 let armed = 0
+radio.setGroup(241)
+let signalOffset = 2
 music.setVolume(255)
 armed = 0
 active = 0
